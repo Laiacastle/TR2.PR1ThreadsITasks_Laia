c3ec87fecb2cdc811e774d8767d31fd3f3d0776d
Laiacastle <laia.castillo.7e6@itb.cat>
2025-05-05 22:09:08 +0200

    Crear i initzialitzar palets

---
+++ TR2.PR1ThreadsITasks_Laia/Palet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TR2.PR1ThreadsITasks_Laia
+{
+    public class Palet
+    {
+        public int Id { get; set; }
+        public bool Agafat { get; set; }
+
+        public Palet(int id, bool agafat)
+        {
+            Agafat = agafat;
+            Id = id;
+        }
+        public override string ToString() => $"Id: {this.Id}, Afagat: {this.Agafat}";
+    }
+}
