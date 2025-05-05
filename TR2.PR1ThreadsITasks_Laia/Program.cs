84e339d02cc219ad04cb7b89d844000b1bcf9070
Laiacastle <laia.castillo.7e6@itb.cat>
2025-05-05 22:02:29 +0200

    Add project files.

---
+++ TR2.PR1ThreadsITasks_Laia/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TR2.PR1ThreadsITasks_Laia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
