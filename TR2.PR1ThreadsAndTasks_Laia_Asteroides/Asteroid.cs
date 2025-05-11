bef152d225e0fd36ba08834811e817748d2d5ed9
Laiacastle <laia.castillo.7e6@itb.cat>
2025-05-11 22:27:29 +0200

    Configurar tasques i afegir objectes

---
+++ TR2.PR1ThreadsAndTasks_Laia_Asteroides/Asteroid.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TR2.PR1ThreadsAndTasks_Laia_Asteroides
+{
+    public class Asteroid: SpaceObject
+    {
+        public bool IsFinalTouched { get; set; }
+
+        public Asteroid(int x, int y) : base(x, y) { IsFinalTouched = false; }
+    }
+}
---
+++ TR2.PR1ThreadsAndTasks_Laia_Asteroides/SpaceObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TR2.PR1ThreadsAndTasks_Laia_Asteroides
+{
+    public class SpaceObject
+    {
+        public int PosX {get;set;}
+        public int PosY {get;set;}
+        
+        public SpaceObject(int x, int y)
+        {
+            PosX = x;
+            PosY = y;
+        }
+    }
+}
